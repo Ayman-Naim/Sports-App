ebe8f1793c72d8544bc28701f41e3c2613a3938d
hebaabdullah1 <127639276+hebaabdullah1@users.noreply.github.com>
2023-09-27 17:53:24 +0300

    Updating UIVersion4 (Leagues Page)

---
+++ SportsApp/Core/LeaguesScreen/Component/SectionHeader/SectionHeader.swift
@@ -0,0 +1,14 @@
+//
+//  SectionHeader.swift
+//  ProjectCollectionn
+//
+//  Created by Shimaa Elcc on 27.9.2023.
+//
+
+import UIKit
+
+class SectionHeader: UICollectionReusableView {
+        
+    @IBOutlet weak var HeaderTitle: UILabel!
+    
+}

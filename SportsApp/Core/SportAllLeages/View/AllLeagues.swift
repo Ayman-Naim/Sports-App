2738b815ac3fa6446f124b33426a94260ea3f2d0
hebaabdullah1 <127639276+hebaabdullah1@users.noreply.github.com>
2023-09-29 21:45:29 +0300

    Using KingFisher and Updating API

--- SportsApp/Core/SportAllLeages/View/AllLeagues.swift
+++ SportsApp/Core/SportAllLeages/View/AllLeagues.swift
@@ -6,15 +6,20 @@
 //
 
 import UIKit
-
+import Kingfisher
 
 
 class AllLeagues: UIViewController , UITableViewDelegate , UITableViewDataSource {
     var LeagesResult : [Leagues]?
     @IBOutlet weak var LeaguesTable: UITableView!
     
-    var viewModel = LeagesViewModel()
+    var viewModel: LeagesViewModel!
     
+    
+    convenience init(viewModel: LeagesViewModel) {
+        self.init()
+        self.viewModel = viewModel
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -36,7 +41,6 @@ class AllLeagues: UIViewController , UITableViewDelegate , UITableViewDataSource
         // Do any additional setup after loading the view.
     }
     
-    
      @IBAction func Back(){
         self.navigationController?.popViewController(animated:true)
     }
@@ -53,7 +57,11 @@ class AllLeagues: UIViewController , UITableViewDelegate , UITableViewDataSource
         cell.FavoriteLabel.text = self.LeagesResult?[indexPath.row].league_name
         //cell.FavoriteImage
         
-        
+        if let imageUrl = URL(string: self.LeagesResult?[indexPath.row].league_logo ?? "") {
+            cell.FavoriteImage.kf.setImage(with: imageUrl)
+        }
+//        cell.youtubeURL = self.LeagesResult?[indexPath.row].youtubeURL
+
         return cell
     }
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
--- SportsApp/Core/SportAllLeages/ViewModel/LeagesViewModel.swift
+++ SportsApp/Core/SportAllLeages/ViewModel/LeagesViewModel.swift
@@ -4,28 +4,47 @@
 //
 //  Created by ayman on 29/09/2023.
 //
-
+//
+//import Foundation
+//
+//
+//class LeagesViewModel{
+//    var LeagesResult :[Leagues]?
+//    init(){
+//        LeagesResult = [Leagues]()
+//
+//    }
+//    func fetch (completionHandeler: @escaping ([Leagues]?) -> Void ){
+//        ApiManger.SharedApiManger.fetchLeagues(url: "https://apiv2.allsportsapi.com/football/?met=Leagues&APIkey=7dbbe4899351e7c403259b7b2f31e9bf9aaba8a00cb18487724163d013402aaf", decodingModel: LeaguesModel.self) { data, error in
+//
+//            print(data)
+//            completionHandeler(data?.result)
+//            self.LeagesResult = data!.result
+//    }
+//
+//
+//
+//
+//    }
+//
+//}
 import Foundation
 
-
-class LeagesViewModel{
-    var LeagesResult :[Leagues]?
-    init(){
-        LeagesResult = [Leagues]()
-        
-    }
-    func fetch (completionHandeler: @escaping ([Leagues]?) -> Void ){
-        ApiManger.SharedApiManger.fetchLeagues(url: "https://apiv2.allsportsapi.com/football/?met=Leagues&APIkey=7dbbe4899351e7c403259b7b2f31e9bf9aaba8a00cb18487724163d013402aaf", decodingModel: LeaguesModel.self) { data, error in
-            
-            print(data)
-            completionHandeler(data?.result)
-            self.LeagesResult = data!.result
-    }
-   
-        
-        
-        
-    }
+class LeagesViewModel {
+    var LeagesResult: [Leagues]?
+    var sport: String?
     
-}
+    init(sport: String) {
+        self.sport = sport
+        LeagesResult = [Leagues]()
+    }
 
+    func fetch(completionHandler: @escaping ([Leagues]?) -> Void) {
+        // Call the fetchLeagues function from ApiManger to fetch data from the API
+        ApiManger.SharedApiManger.fetchLeagues(url: "https://apiv2.allsportsapi.com/\(sport ?? "")/?met=Leagues&APIkey=7dbbe4899351e7c403259b7b2f31e9bf9aaba8a00cb18487724163d013402aaf", decodingModel: LeaguesModel.self) { data, error in
+            // Handle the API response
+            completionHandler(data?.result)
+            self.LeagesResult = data?.result
+        }
+    }
+}
